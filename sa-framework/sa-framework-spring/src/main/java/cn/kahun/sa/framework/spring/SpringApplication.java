b7178a4b7be617afd01a8d57769cb56c1c065f7d
liboah <liboah@yonyou.com>
2020-03-27 17:27:37 +0800

    ...

--- sa-framework/sa-framework-spring/src/main/java/cn/kahun/sa/framework/spring/SpringApplication.java
+++ sa-framework/sa-framework-spring/src/main/java/cn/kahun/sa/framework/spring/SpringApplication.java
@@ -1,8 +1,13 @@
 package cn.kahun.sa.framework.spring;
 
+import com.alibaba.fastjson.JSON;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 
 /**
  * @author kahun
@@ -10,12 +15,19 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class SpringApplication {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        new SpringApplicationBuilder(SpringApplication.class)
-                .web(WebApplicationType.SERVLET)
-                .run(args);
+    new SpringApplicationBuilder(SpringApplication.class)
+        .web(WebApplicationType.SERVLET)
+        .bannerMode(Mode.OFF)
+        .logStartupInfo(false)
+        .listeners(event -> {
+          System.out.println("****************");
+          System.out.println(event);
+          System.out.println();
+        })
+        .run(args);
 
-    }
+  }
 
 }
