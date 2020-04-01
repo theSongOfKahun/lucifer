8f6a39792cd8cce7ad495595297afd619799b1a1
= <=>
2020-04-01 21:52:01 +0800

    spring security

--- dt-security/src/main/java/cn/kahun/dt/security/config/WebSecurityConfig.java
+++ dt-security/src/main/java/cn/kahun/dt/security/config/WebSecurityConfig.java
@@ -1,47 +1,47 @@
-package cn.kahun.dt.security.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-/**
- * @author libo
- * @date 2020/2/22 22:35
- * @description 记得写注释
- */
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-
-    http.authorizeRequests(
-        authorizeRequestsCustomizer ->
-            authorizeRequestsCustomizer
-                .antMatchers("/css/**", "/index").permitAll()
-                .antMatchers("/user/**").hasRole("USER"))
-        .formLogin(
-            httpSecurityFormLoginConfigurer ->
-                httpSecurityFormLoginConfigurer
-                    .loginPage("/login")
-                    .failureUrl("/login-error"));
-  }
-
-  @Bean
-  @Override
-  public UserDetailsService userDetailsService() {
-
-    UserDetails userDetails = User.withDefaultPasswordEncoder()
-        .username("user")
-        .password("password")
-        .roles("USER")
-        .build();
-
-    return new InMemoryUserDetailsManager(userDetails);
-  }
-}
+//package cn.kahun.dt.security.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+///**
+// * @author libo
+// * @date 2020/2/22 22:35
+// * @description 记得写注释
+// */
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//
+//    http.authorizeRequests(
+//        authorizeRequestsCustomizer ->
+//            authorizeRequestsCustomizer
+//                .antMatchers("/css/**", "/index").permitAll()
+//                .antMatchers("/user/**").hasRole("USER"))
+//        .formLogin(
+//            httpSecurityFormLoginConfigurer ->
+//                httpSecurityFormLoginConfigurer
+//                    .loginPage("/login")
+//                    .failureUrl("/login-error"));
+//  }
+//
+//  @Bean
+//  @Override
+//  public UserDetailsService userDetailsService() {
+//
+//    UserDetails userDetails = User.withDefaultPasswordEncoder()
+//        .username("user")
+//        .password("password")
+//        .roles("USER")
+//        .build();
+//
+//    return new InMemoryUserDetailsManager(userDetails);
+//  }
+//}
