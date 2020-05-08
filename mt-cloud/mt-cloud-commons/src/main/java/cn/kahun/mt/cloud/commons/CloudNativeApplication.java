f2332d746116a71ef9c6d8dbc55b758a9b4c307a
liboah <liboah@yonyou.com>
2020-05-08 17:55:07 +0800

    cloud commons

---
+++ mt-cloud/mt-cloud-commons/src/main/java/cn/kahun/mt/cloud/commons/CloudNativeApplication.java
@@ -0,0 +1,18 @@
+package cn.kahun.mt.cloud.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @create: 2020/5/7 11:27 上午
+ * @author: liboah
+ * @description: 记得写注释。。。
+ **/
+@SpringBootApplication
+public class CloudNativeApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(CloudNativeApplication.class,args);
+  }
+}
